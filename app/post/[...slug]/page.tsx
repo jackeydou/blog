5a1d9048d21e6d5455363dc8148e2da21be076f7
XHeapStack <jackey.dou@gmail.com>
2023-08-14 22:05:12 +0800

    fix: post/[...slug] light mode ui bug

--- app/post/[...slug]/page.tsx
+++ app/post/[...slug]/page.tsx
@@ -53,7 +53,7 @@ export default async function Post({ params }: { params: { slug: string[] } }) {
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-4xl xl:px-0 flex min-h-screen flex-col pb-10">
       <TopNav className="lg:flex" />
-      <div className="animate-[fadein-b_1s_ease-in-out]">
+      <div className="animate-[fadein-b_1s_ease-in-out] z-10">
         <MDX sourcePath={mdPath} />
       </div>
       <MobileNav className="lg:hidden" />
